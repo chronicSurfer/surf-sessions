e7fda1b726464483bc08a9ce32cfb85c67de1b02
chronicSurfer <kyle.umbarger@gmail.com>
2019-04-09 15:18:00 -0700

    Creating buttons

--- script.js
+++ script.js
@@ -11,14 +11,15 @@ var addButton = $('#submittion').click(()=>{
     var location = $('input#location').val();
     var size = $('input#size').val();
     var rating = $('input#rating').val();
-    var deleteButton = $('<button/>', {
-        type: 'button',
-        name: 'deleteButton',
-        class: 'btn cancel red',
-        id: 'deleteButton'
-    });
     var tr = '<tr><td>'+date+'</td><td>'+location+'</td><td>'+size+'</td><td>'+rating+'</td><td>'+deleteButton+'</td></tr>';
     $('tbody').append(tr);
 });
 
 var clearButton;
+
+var deleteButton = $('<button/>', {
+    type: 'button',
+    name: 'deleteButton',
+    class: 'btn cancel red',
+    id: 'deleteButton'
+});
